bbe5d02cf9e612c22e987465cda07080ced7ddcb
aryan <indianpiear@gmail.com>
2025-06-14 04:34:00 +0530

    Initial commit

--- .next/static/chunks/src_app_contact_page_tsx_604fab72._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e72512ab._.js",
-  "static/chunks/src_749d9e06._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__ecfa1faf._.css",
-  "static/chunks/_6a5bb7cf._.js"
-],
-    source: "dynamic"
-});
